10e0b9361a36d133a05bbc95ce7076a7a9cc90d1
Daniel Esteban <conejo@conejo.me>
2020-01-19 17:37:39 +0100

    added pybadge example

---
+++ examples/pybadge/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"image/color"
+	"machine"
+
+	"github.com/conejoninja/tinydraw"
+
+	"tinygo.org/x/drivers/st7735"
+)
+
+func main() {
+	machine.SPI1.Configure(machine.SPIConfig{
+		SCK:       machine.SPI1_SCK_PIN,
+		MOSI:      machine.SPI1_MOSI_PIN,
+		MISO:      machine.SPI1_MISO_PIN,
+		Frequency: 8000000,
+	})
+
+	display := st7735.New(machine.SPI1, machine.TFT_RST, machine.TFT_DC, machine.TFT_CS, machine.TFT_LITE)
+	display.Configure(st7735.Config{
+		Rotation: st7735.ROTATION_90,
+	})
+
+	display.FillScreen(color.RGBA{255, 255, 255, 255})
+
+	tinydraw.Line(&display, 10, 10, 94, 10, getRainbowRGB(0))
+	tinydraw.Line(&display, 94, 16, 10, 16, getRainbowRGB(15))
+	tinydraw.Line(&display, 10, 20, 10, 118, getRainbowRGB(30))
+	tinydraw.Line(&display, 16, 118, 16, 20, getRainbowRGB(45))
+
+	tinydraw.Line(&display, 40, 40, 80, 80, getRainbowRGB(60))
+	tinydraw.Line(&display, 40, 40, 80, 70, getRainbowRGB(75))
+	tinydraw.Line(&display, 40, 40, 80, 60, getRainbowRGB(90))
+	tinydraw.Line(&display, 40, 40, 80, 50, getRainbowRGB(105))
+	tinydraw.Line(&display, 40, 40, 80, 40, getRainbowRGB(120))
+
+	tinydraw.Line(&display, 100, 100, 40, 100, getRainbowRGB(135))
+	tinydraw.Line(&display, 100, 100, 40, 90, getRainbowRGB(150))
+	tinydraw.Line(&display, 100, 100, 40, 80, getRainbowRGB(165))
+	tinydraw.Line(&display, 100, 100, 40, 70, getRainbowRGB(180))
+	tinydraw.Line(&display, 100, 100, 40, 60, getRainbowRGB(195))
+	tinydraw.Line(&display, 100, 100, 40, 50, getRainbowRGB(210))
+
+	tinydraw.Rectangle(&display, 30, 106, 120, 20, getRainbowRGB(225))
+	tinydraw.FilledRectangle(&display, 34, 110, 112, 12, getRainbowRGB(130))
+
+	tinydraw.Circle(&display, 120, 30, 20, getRainbowRGB(240))
+	tinydraw.FilledCircle(&display, 120, 30, 16, getRainbowRGB(145))
+
+	tinydraw.Triangle(&display, 120, 102, 100, 80, 152, 46, getRainbowRGB(155))
+	tinydraw.FilledTriangle(&display, 120, 98, 104, 80, 144, 54, getRainbowRGB(90))
+
+	display.Display()
+
+}
+
+func getRainbowRGB(i uint8) color.RGBA {
+	if i < 85 {
+		return color.RGBA{i * 3, 255 - i*3, 0, 255}
+	} else if i < 170 {
+		i -= 85
+		return color.RGBA{255 - i*3, 0, i * 3, 255}
+	}
+	i -= 170
+	return color.RGBA{0, i * 3, 255 - i*3, 255}
+}
