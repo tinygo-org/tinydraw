9b56e6600416c935afff0111288360d10da3667b
Ron Evans <ron@hybridgroup.com>
2020-04-13 10:24:01 +0200

    all: add Version to package
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

---
+++ version.go
@@ -0,0 +1,7 @@
+package tinydraw
+
+// Version returns a user-readable string showing the version of the tinydraw package
+// for support purposes.
+//
+// Update this value before release of new version of software.
+const Version = "0.2.0"
